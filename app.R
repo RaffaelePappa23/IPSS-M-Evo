1725550bcb57210d6c6c5bc6dee8b19884de7f2a
RaffaelePappa23 <raffaele.pappa23@gmail.com>
2025-05-31 12:00:12 +0200

    Piccolo aggiornamento prima versione

--- app.R
+++ app.R
@@ -1,12 +1,9 @@
 # ─────────────────────────────────────────────────────────────────────────────
 # app.R
-# Punto di ingresso: carica `global.R`, `ui.R`, `server.R` e avvia l’app.
 # ─────────────────────────────────────────────────────────────────────────────
 
 source("global.R")
-
 source("ui.R")
-
 source("server.R")
 
 shinyApp(ui = ui, server = server)
